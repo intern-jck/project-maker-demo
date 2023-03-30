ec97fbad0d7c3df67e4a0b76224e9b987fac0716
Justin C Kirk <justin.c.kirk@gmail.com>
2023-03-30 09:53:24 -0400

    cleans barrel

--- src/common/components/Inputs/index.ts
+++ src/common/components/Inputs/index.ts
@@ -1,5 +1,3 @@
-// import styles from '@/styles/components/Inputs/Inputs.module.scss';
-
 import TextInput from './TextInput';
 import TextArea from './TextArea';
 import DateInput from './DateInput';
