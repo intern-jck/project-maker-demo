51c74d0e8592bb01ac2291c9a9479f00c191b81e
Justin C Kirk <justin.c.kirk@gmail.com>
2023-04-11 11:52:52 -0400

    completes refactor mvp

--- src/common/modules/utils/getProjects.ts
+++ src/common/modules/utils/getProjects.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export default async function getProjects(collectionId: string = '') {
+export default async function getProjects(folderId: string = '') {
   try {
-    const response = axios.get(`/api/projects?collectionId=${collectionId}`);
+    const response = axios.get(`/api/projects?folderId=${folderId}`);
     const _projects = await response;
     return _projects.data;
   } catch (error) {
